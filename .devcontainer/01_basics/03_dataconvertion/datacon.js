68f603ebbeefc821afe92726543b052ee4814f8b
RishabhD30 <150004882+RishabhD30@users.noreply.github.com>
2024-04-01 10:52:22 +0000

    DataCon done

---
+++ .devcontainer/01_basics/03_dataconvertion/datacon.js
@@ -0,0 +1,14 @@
+let score = "10abc";
+let code = 1;
+
+let x = Boolean(code);
+console.log(typeof score); //string
+console.log(score); 10abc
+console.log(code);
+console.log(x); // 1=>True 0=>false in Boolean 
+
+let val = Number(score);
+
+console.log(typeof val);
+console.log(val);
+
