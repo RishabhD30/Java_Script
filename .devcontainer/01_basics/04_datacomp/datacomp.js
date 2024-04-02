7047873462ed69527a85b9b297ad1a8032ccbf13
RishabhD30 <150004882+RishabhD30@users.noreply.github.com>
2024-04-02 06:21:10 +0000

    Datacomp

---
+++ .devcontainer/01_basics/04_datacomp/datacomp.js
@@ -0,0 +1,16 @@
+// console.log("2"+1);
+
+// console.log(1+3+'2');
+
+// console.log('1'+3+1);
+
+console.log(null<0);//false
+console.log(null==0); //false
+console.log(null>0); //false
+console.log(null>=0); //true
+
+console.log(undefined == 0 ); //false
+console.log(undefined > 0); //false
+console.log(undefined < 0); //false
+
+console.log("2" === 2); //false
