a8ee5dadfd5d5bcb6a46254b3f45f4efbdd42a27
RishabhD30 <150004882+RishabhD30@users.noreply.github.com>
2024-04-03 08:35:43 +0000

    Con

---
+++ .devcontainer/01_basics/07_String/string.js
@@ -0,0 +1,21 @@
+const name = "Rishabh";
+const repocount = 50;
+
+console.log(`My name is ${name} and my repo count is ${repocount}`) ;
+
+const game_name = new String('Hello World');
+
+
+/**
+console.log(game_name[0]);
+// console.log(game_name.); // when u put dot after the String Variable then u can check all the Prototypes
+console.log(game_name.toUpperCase())
+console.log(game_name.length);
+
+console.log(game_name.charAt(2)); // CharAt funtion or prototype used for find what char is present at index mentioned in the paranthesis
+console.log(game_name.indexOf('W')); //Give u the output indexof Function it tells in which index the char is present. 
+ */
+
+const newString = game_name.substring(0,4)
+console.log(newString);
+
