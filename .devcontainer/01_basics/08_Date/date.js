32fb5db095db14b6b801ba1ebb0149f4c9d6fe75
RishabhD30 <150004882+RishabhD30@users.noreply.github.com>
2024-04-05 11:12:13 +0000

    Done

---
+++ .devcontainer/01_basics/08_Date/date.js
@@ -0,0 +1,13 @@
+let mydate = new Date();
+
+// console.log(mydate);
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleString());
+// console.log(typeof mydate);
+
+let mynewdate = new Date("2024 04 05");
+// console.log(mynewdate.toDateString());
+
+let Timestamp = Date.now();
+console.log(Math.floor(Date.now()/1000)); 
