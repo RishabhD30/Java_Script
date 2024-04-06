b7234b502258692269c2d4528592928f0ac634d1
RishabhD30 <150004882+RishabhD30@users.noreply.github.com>
2024-04-06 12:14:44 +0000

    doneee

---
+++ .devcontainer/01_basics/09_Array/array.js
@@ -0,0 +1,11 @@
+const arr = [2, 4, 6, 8 ,10];
+// console.log(arr); // PRint full array 
+// console.log(arr[3]); // for one element
+
+// console.log(arr.length);
+// console.log(arr.push(20));
+// console.log(arr);
+// console.log(arr.pop(20));
+// console.log(arr);
+console.log(arr.length);
+console.log(arr.keys());
