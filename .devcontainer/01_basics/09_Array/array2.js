6c7facbc1ec2935c45ad6d7a6929086c6d6d3dac
RishabhD30 <150004882+RishabhD30@users.noreply.github.com>
2024-04-08 07:37:17 +0000

    array2 comit

---
+++ .devcontainer/01_basics/09_Array/array2.js
@@ -0,0 +1,13 @@
+const a1 = ["apple", "banana" , 'grapes',"guava"]
+const a2 = ["Rishabh" , "Dwivedi" , "javascript"]
+
+/* Concat or Spread function */  //Both returns same 
+//Concat and spread both are used for to add to arrays element into one  and work return a new Array.
+// const addArr = a1.concat(a2);
+// console.log(addArr);
+
+        // OR
+
+// const spreadarr = [...a1, ...a2];
+// console.log(spreadarr);
+
