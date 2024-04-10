abf9a358e9c89ac4e82f3e5643bc3771a484f027
RishabhD30 <150004882+RishabhD30@users.noreply.github.com>
2024-04-10 09:46:25 +0000

    Object2

---
+++ .devcontainer/03_object/02_object.js
@@ -0,0 +1,37 @@
+// const users = new Object(); // Two ways to create objects 
+const users = {} // In this line another way to create it.
+
+users.id="KEI003730"
+users.email="Rishabh.dwivedi@kei-ind.com"
+// console.log(users);
+
+const regularuser = {
+    name : "hek",
+    dept : {
+        ITusers:{
+            fullname:{
+                ITfullname:"RISHABH Dwivedi",
+                empid: "KEI3730",
+            },
+        },
+        managers:{
+            manfullname:{name:"Nilesh"}
+        },
+    },
+}
+// console.log(regularuser.dept.managers.manfullname); // We can easily Access kr sakte h by using dots
+
+/** Another Case  */
+
+const obj1 = {1:"a" , 2:"b", 3:"c"}
+const obj2= {4:"d" , 5:"e"}
+
+/* different ways */
+const a = {obj1,obj2}  // Output will be -->  { obj1: { '1': 'a', '2': 'b', '3': 'c' }, obj2: { '4': 'd', '5': 'e' } } //Not Good
+const b = Object.assign({},obj1,obj2) // Output will be --> { '1': 'a', '2': 'b', '3': 'c', '4': 'd', '5': 'e' } // this will concat both the object
+console.log(a);
+
+/* Case - 2  */
+const abc = [...obj1, ...obj2]
+console.log(abc);
+
