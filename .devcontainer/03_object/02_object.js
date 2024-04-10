bfdfff5b711a1b38a9aae6d35cc27067c0ce0739
RishabhD30 <150004882+RishabhD30@users.noreply.github.com>
2024-04-10 10:11:05 +0000

    Object2

--- .devcontainer/03_object/02_object.js
+++ .devcontainer/03_object/02_object.js
@@ -29,9 +29,28 @@ const obj2= {4:"d" , 5:"e"}
 /* different ways */
 const a = {obj1,obj2}  // Output will be -->  { obj1: { '1': 'a', '2': 'b', '3': 'c' }, obj2: { '4': 'd', '5': 'e' } } //Not Good
 const b = Object.assign({},obj1,obj2) // Output will be --> { '1': 'a', '2': 'b', '3': 'c', '4': 'd', '5': 'e' } // this will concat both the object
-console.log(a);
+// console.log(a);
 
 /* Case - 2  */
-const abc = [...obj1, ...obj2]
-console.log(abc);
+    // const abc = [...obj1, ...obj2]
+    // console.log(abc);
 
+const user = [
+    {
+        id:1,
+        email:"rish@gmail.com"
+    },
+    {
+        id:2,
+        email:"R@gmail.com"
+    },
+    {
+        id:1,
+        email:"Dwivedi@gmail.com"
+    },
+]
+
+console.log(users);
+console.log(Object.keys(users)); // Kisi bhi object ki sari  Keys print krdeta h 
+console.log(Object.values(users)); // kisi bhi object ki sari values print krdeta h
+console.log(Object.entries(users)); // This will convert every key value in a array n seperated by " , "  in return as Array  
