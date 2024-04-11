d14649507228236cadc54d322fe1c56a2de65d28
RishabhD30 <150004882+RishabhD30@users.noreply.github.com>
2024-04-11 11:55:25 +0000

    scope

--- .devcontainer/04_Functions/this.js
+++ .devcontainer/04_Functions/this.js
@@ -2,7 +2,7 @@ const users = {
     emp:"KEI3730",
     price:500,
     printemp:function(){
-        console.log(`${.emp}, is my employee id`);
+        console.log(`${this.emp}, is my employee id`);
     }
 }
-one(users);
+printemp(users);
