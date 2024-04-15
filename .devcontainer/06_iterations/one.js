023b5801bcca7cf3c49c77a4351996b071d9dc63
RishabhD30 <150004882+RishabhD30@users.noreply.github.com>
2024-04-15 07:40:53 +0000

    For  Loop Done

---
+++ .devcontainer/06_iterations/one.js
@@ -0,0 +1,14 @@
+// In this we will Study About the For loops
+
+const arr = ["flash" , "batman" , "ironman"]
+
+for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    console.log(`Heros: ${element}`);
+}
+
+// 3 Keywords are also performed such as Break , Continue , Pass
+
+// Break ->(The break statement "jumps out" of a loop) Break Keyword do stop all the iteration after the condition then we will out from the Loop
+// Continue -> In this keyword accept the condition given all the iteration will work. 
+// Pass -> It is used like is we forget something the execution process throw an error so it didnt throw after the pass keyword.
