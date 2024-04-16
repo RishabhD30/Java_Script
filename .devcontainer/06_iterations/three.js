46d0ae20dd0ce3edcd5d69f6b05d9ca4f9c5e845
RishabhD30 <150004882+RishabhD30@users.noreply.github.com>
2024-04-16 05:32:07 +0000

    Loops

--- .devcontainer/06_iterations/three.js
+++ .devcontainer/06_iterations/three.js
@@ -1,6 +1,9 @@
+/* 3 types of For loops--> (i) For  , (ii) forof(VALUES), (iii) forin(KEYS) , (iv) forEach(different method)*/
+
+// ForOf
 const arr = [1,2,3,5,10] ;
 for (const i of arr) {
-    console.log(i);
+    // console.log(i);
 }
 
 // MAP KeywordS
@@ -11,5 +14,34 @@ map.set(2,"Dwi")
 map.set(3,"Hello")
 
 for (const [i,val] of map) {
-    console.log(i,":-",val);
+    // console.log(i,":-",val);
 }
+
+const hey = new Map();
+map.set(1,"hel")
+map.set(2,"wo")
+map.set(3,"orld")
+
+
+// ForIn loop
+const newobj  = {js:"javascript", cpp: "c++" , RD : "Rishabh Dwivedi "}
+for (const key in newobj) {
+    // console.log(key);
+}
+
+//ForEach
+const likeArr = [1,2,3,5,10] ;
+// likeArr.forEach(function (val){ 
+//     console.log(val);
+// }); 
+
+// ANother way to return this output with array function 
+// likeArr.forEach((val)=>{
+//     console.log(val);
+// })
+
+//3 way to use Foreach
+function printme(val) {
+    console.log(val);
+}
+likeArr.forEach(printme);
